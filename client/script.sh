e12a92f3a62dc88089310ed4e6c237cca22b9f07
Paul Arelt <paularelt@gmail.com>
2022-06-23 17:50:58 -0400

    Correct some things

--- client/script.sh
+++ client/script.sh
@@ -3,9 +3,12 @@
 server_host=${server_host}
 server_port=${server_port}
 
+echo "server_host ${server_host}"
+echo "server_port ${server_port}"
+
 while [ 1 ];
 do
-    curl -XGET server_host:server_port
+    curl -XGET $server_host:$server_port
     sleep 3
 done
 
