f8b4d433f72c47b59c2784f58f6d710ef52bd591
Paul Arelt <paularelt@gmail.com>
2022-06-25 15:41:14 -0400

    Tweaks to environment variables and client app

--- client/script.sh
+++ client/script.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-server_host=${server_host}
-server_port=${server_port}
-
-echo "server_host ${server_host}"
-echo "server_port ${server_port}"
-
 while [ 1 ];
 do
-    curl -XGET $server_host:$server_port
+    curl $server_host:$server_port >/dev/null
     sleep 3
 done
 
